f55c7af039fad6ee388418bdbc6ba93dc88047e0
shipperizer <alexcabb@gmail.com>
2017-11-13 16:13:25 +0000

    [++] use direct exchange and drop fanout

--- silver_giggle/consumer/__init__.py
+++ silver_giggle/consumer/__init__.py
@@ -8,14 +8,8 @@ from silver_giggle.celery import init_celery
 def init_consumer():
     app = init_celery('consumer.{}'.format(environ['CELERY_NAME']), 'silver_giggle.consumer')
 
-    # one per service
-    exchange_name = 'silver_giggle.consumer.{}'.format(environ['CELERY_NAME'])
-    service_exchange = Exchange(exchange_name, type='direct', channel=app.broker_connection().channel())
-    service_exchange.declare()
-    service_exchange.bind_to('silver_giggle.producer.broadcast')
-
     app.conf.task_queues = [
-        Queue(app.main, Exchange(exchange_name, type='direct')),
+        Queue(app.main, Exchange('silver_giggle.producer.broadcast'), routing_key='msg')
     ]
 
     return app
--- silver_giggle/producer/__init__.py
+++ silver_giggle/producer/__init__.py
@@ -10,7 +10,7 @@ def init_producer():
 
     channel = app.broker_connection().channel()
 
-    broadcast_exchange = Exchange('silver_giggle.producer.broadcast', type='fanout', channel=channel)
+    broadcast_exchange = Exchange('silver_giggle.producer.broadcast', channel=channel)
     broadcast_exchange.declare()
 
     app.conf.task_queues = (
@@ -20,15 +20,19 @@ def init_producer():
 
     app.conf.task_routes = {
         'silver_giggle.producer.tasks.log': 'silver_giggle.producer',
-        'silver_giggle.consumer.tasks.log': 'silver_giggle.producer.broadcast',
+        'silver_giggle.consumer.tasks.log': {
+            'queue': 'silver_giggle.producer.broadcast',
+            'exchange': 'silver_giggle.producer.broadcast',
+            'routing_key': 'msg'
+        }
     }
 
     app.conf.beat_schedule = {
         'log': {
             'task': 'silver_giggle.producer.tasks.log',
-            'schedule': timedelta(seconds=5),
+            'schedule': timedelta(seconds=1),
             'args': (),
-        },
+        }
     }
 
     return app
